5b60abd3c7ec7ba1e61d8c4bf92d9dafb7eb7d01
Pedro Barco <pedro.barco@tecnico.ulisboa.pt>
2025-07-30 23:27:11 +0100

    feat(agent): configure server to use net/http

--- cmd/mroki-agent/main.go
+++ cmd/mroki-agent/main.go
@@ -1,15 +1,64 @@
 package main
 
 import (
+	"flag"
 	"log/slog"
+	"net/http"
+	"net/url"
 
-	caddycmd "github.com/caddyserver/caddy/v2/cmd"
-
-	// Import the mroki caddy module
-	_ "github.com/pedrobarco/mroki/pkg/caddymodule"
+	"github.com/pedrobarco/mroki/pkg/proxy"
 )
+
+var (
+	fLive   string
+	fShadow string
+)
+
+func init() {
+	flag.StringVar(&fLive, "live", "", "Live URL to proxy requests to")
+	flag.StringVar(&fShadow, "shadow", "", "Shadow URL to proxy requests to")
+	flag.Parse()
+}
 
 func main() {
 	slog.SetLogLoggerLevel(slog.LevelDebug)
-	caddycmd.Main()
+
+	if fLive == "" {
+		slog.Error("Live URL is required")
+		return
+	}
+
+	if fShadow == "" {
+		slog.Error("Shadow URL is required")
+		return
+	}
+
+	live, err := url.Parse(fLive)
+	if err != nil {
+		slog.Error("Invalid live URL", "error", err)
+		return
+	}
+
+	shadow, err := url.Parse(fShadow)
+	if err != nil {
+		slog.Error("Invalid shadow URL", "error", err)
+		return
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", proxy.NewProxy(live, shadow))
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	slog.Info("Started server",
+		"live", live.String(),
+		"shadow", shadow.String(),
+		"address", server.Addr,
+	)
+	if err := server.ListenAndServe(); err != nil {
+		slog.Error("Failed to start server", "error", err)
+		return
+	}
 }
