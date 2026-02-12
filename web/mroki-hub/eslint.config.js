0665f6167c598e48fcdccefaebb38e9664aeebc7
Pedro Barco <pedro.barco@tecnico.ulisboa.pt>
2026-02-12 22:34:58 +0000

    style(web): configure ESLint and Prettier for mroki-hub
    - Add ESLint with flat config for Vue 3 + TypeScript
    - Configure Prettier with project code style preferences
    - Ignore src/components/ui/ from linting (shadcn-vue managed)
    - Add lint and format scripts to package.json
    
    ESLint plugins:
    - eslint-plugin-vue
    - @typescript-eslint/eslint-plugin
    - eslint-plugin-prettier
    
    Code style:
    - Single quotes, no semicolons
    - 100 character line width
    - LF line endings

---
+++ web/mroki-hub/eslint.config.js
@@ -0,0 +1,51 @@
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import tseslint from 'typescript-eslint'
+import prettier from 'eslint-plugin-prettier/recommended'
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  prettier,
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+  },
+  {
+    rules: {
+      // Vue-specific rules
+      'vue/multi-word-component-names': 'off',
+      'vue/require-default-prop': 'off',
+      
+      // TypeScript rules
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { 
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_' 
+      }],
+      
+      // General rules
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-debugger': 'warn',
+    },
+  },
+  {
+    ignores: [
+      'dist/**',
+      'node_modules/**',
+      '*.config.js',
+      '*.config.ts',
+      '.vite/**',
+      'src/components/ui/**',
+    ],
+  }
+)
+
